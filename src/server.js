8b61c5b72c5c8f366a36de3f61601d212b445a3a
mksigrest <mksigrest@gmail.com>
2025-09-21 11:54:45 -0400

    first draft code written

--- src/server.js
+++ src/server.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
     const parsedUrl = url.parse(request.url, true);
     const path = parsedUrl.pathname;
+    const query = parsedUrl.pathname;
+
+    let accept = request.headers.accept || 'application/json';
 
     if (path === '/' || path === '/client.html') {
         fs.readFile('index.html', (error, data) => {
@@ -18,6 +22,62 @@ const server = http.createServer((request, response) => {
             response.end(data);
         });
         return;
+    }
+
+    else if (path === 'style.css') {
+        fs.readFile('style.css', (error, data) => {
+            if (error) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Not found');
+                return;
+            }
+
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            response.end(data);
+        });
+        return;
+    }
+
+    else if (path === '/success') {
+        if (accept.includes('text/xml')) {
+            response.writeHead(statusCode, { 'Content-Type': 'text/xml' });
+            response.end(`<response><message>This request was a success</message></response>`)
+        }
+
+        else {
+            response.writeHead(200, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify({message: "This request was a success"}));
+        }
+    }
+
+    else if (path === '/badRequest') {
+        if (accept.includes('text/xml')) {
+            response.writeHead(statusCode, { 'Content-Type': 'text/xml' });
+            response.end(`<response><message>Missing valid query parameter</message><id>badRequest</id></response>`);
+        }
+
+        else {
+            response.writeHead(400, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify({
+                message: "Missing valid query parameter",
+                id: "badRequest"
+            }));
+        }
+    }
+
+    else if (path === '/unauthorized') {
+        if (accept.includes('text/xml')) {
+            response.writeHead(statusCode, { 'Content-Type': 'text/xml' });
+            response.end(`<response><message>Request was unauthorized</message><id>unauthorized</id></response>`);
+        }
+
+        else {
+            response.writeHead(401, { 'Content-Type': 'application/json' });
+            response.end(JSON.stringify({
+                message: "Request was unauthorized",
+                id: "unauthorized"
+            }))
+        }
     }
 });
 
