fd33ba6cdc47b7ba54be9b5039cee9c5d6df81d6
mksigrest <mksigrest@gmail.com>
2025-09-21 14:31:04 -0400

    Close to done
    Looks finished, doing a final round of debugging and checking if everything is good

--- client/server.js
+++ client/server.js
@@ -10,7 +10,7 @@ const responseJSON = (response, statusCode, object) => {
 };
 
 const responseXML = (response, statusCode, xmlString) => {
-    response.writeHead(statusCode, { 'Content-Type ': 'text/xml' });
+    response.writeHead(statusCode, { 'Content-Type': 'text/xml' });
     response.end(xmlString);
 }
 
@@ -50,77 +50,77 @@ const server = http.createServer((request, response) => {
 
     else if (path === '/success') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 200, `<response><message>This request was a success</message></response>`);
+            responseXML(response, 200, `<response><message>This is a successful response.</message></response>`);
         }
 
         else {
-            responseJSON(response, 200, { message: "This request was a success" });
+            responseJSON(response, 200, { message: "This is a successful response." });
         }
         return;
     }
 
     else if (path === '/badRequest') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 400, `<response><message>Missing valid query parameter</message><id>badRequest</id></response>`);
+            responseXML(response, 400, `<response><message>Missing valid query parameter set to true.</message><id>badRequest</id></response>`);
         }
 
         else {
-            responseJSON(response, 400, { message: "Missing valid query parameter", id: "badRequest" });
+            responseJSON(response, 400, { message: "Missing valid query parameter set to true.", id: "badRequest" });
         }
         return;
     }
 
     else if (path === '/unauthorized') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 401, `<response><message>Request was unauthorized</message><id>unauthorized</id></response>`);
+            responseXML(response, 401, `<response><message>Missing loggedIn query parameter set to yes.</message><id>unauthorized</id></response>`);
         }
 
         else {
-            responseJSON(response, 401, { message: "Request was unauthorized", id: "unauthorized" })
+            responseJSON(response, 401, { message: "Missing loggedIn query parameter set to yes.", id: "unauthorized" })
         }
         return;
     }
 
     else if (path === '/forbidden') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 403, `<response><message>Forbidden</message><id>forbidden</id></response>`);
+            responseXML(response, 403, `<response><message>You do not have access to this content.</message><id>forbidden</id></response>`);
         }
 
         else {
-            responseJSON(response, 403, { message: "Forbidden", id: "forbidden" })
+            responseJSON(response, 403, { message: "You do not have access to this content.", id: "forbidden" })
         }
         return;
     }
 
     else if (path === '/internal') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 500, `<response><message>Internal server error</message><id>internal</id></response>`);
+            responseXML(response, 500, `<response><message>Internal server error. Something went wrong.</message><id>internalError</id></response>`);
         }
 
         else {
-            responseJSON(response, 500, { message: "Internal server error", id: "internal" })
+            responseJSON(response, 500, { message: "Internal server error. Something went wrong.", id: "internalError" })
         }
         return;
     }
 
-    else if (path === '/internal') {
+    else if (path === '/notImplemented') {
         if (accept.includes('text/xml')) {
-            responseXML(response, 501, `<response><message>Not implimented</message><id>notImplimented</id></response>`);
+            responseXML(response, 501, `<response><message>A get request for this page has not been implemented yet. Check again later for updated content.</message><id>notImplimented</id></response>`);
         }
 
         else {
-            responseJSON(response, 501, { message: "Not implimented", id: "notImplimented" })
+            responseJSON(response, 501, { message: "A get request for this page has not been implemented yet. Check again later for updated content.", id: "notImplimented" })
         }
         return;
     }
 
     else {
         if (accept.includes('text/xml')) {
-            responseXML(response, 404, `<response><message>Page not found</message><id>notFound</id></response>`);
+            responseXML(response, 404, `<response><message>The page you are looking for was not found.</message><id>notFound</id></response>`);
         }
 
         else {
-            responseJSON(response, 404, { message: "Page not found", id: "notFound" })
+            responseJSON(response, 404, { message: "The page you are looking for was not found.", id: "notFound" })
         }
         return;
     }
